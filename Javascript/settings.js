5f73c7a0b4831aa3a2d9199f4fa75ff7223ed034
Francisco <fjab92@gmail.com>
2017-04-12 00:23:38 -0600

    Forgot to commit the settings file

---
+++ Javascript/settings.js
@@ -0,0 +1,248 @@
+let Settings = (function() {
+    const textTopSpace = 30;
+    const rectTopSpace = 30;
+    const canvas = document.getElementById('canvas');
+
+    let inputDispatch = {
+        'LEFT': {
+            'keycode': 37,
+        },
+        'UP': {
+            'keycode': 38,
+        },
+        'RIGHT': {
+            'keycode': 39,
+        },
+        'DOWN': {
+            'keycode': 40,
+        }
+    };
+
+    let music = {
+        font: "16px Arial",
+        color: "#FFFFFF",
+        text: 'Tie Fighter Sound: ON',
+        x: 50,
+        y: 50
+    };
+
+    function update(elapsedTime) {
+
+    }
+
+    const defaultFillStyle = 'rgba(225, 225, 225, 0.5)';
+    const highlightFillStyle = 'rgba(225, 225, 225, 1.0)';
+
+    const moveLeftRow = {
+        direction: 'LEFT',
+        isHighlighted: false,
+        text: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'MOVE LEFT:',
+            x: 40,
+            y: 75
+        },
+        controls: {
+            x: 175,
+            y: 58,
+            width: 225,
+            height: 22,
+            fillStyle: defaultFillStyle,
+        },
+        keyPosition: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'ArrowLeft',
+            x: 175 + 225 / 2 - 30,
+            y: 75
+        }
+    };
+
+    const moveRightRow = {
+        direction: 'RIGHT',
+        isHighlighted: false,
+        text: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'MOVE RIGHT:',
+            x: 40,
+            y: moveLeftRow.text.y + textTopSpace
+        },
+        controls: {
+            x: 175,
+            y: moveLeftRow.controls.y + rectTopSpace,
+            width: 225,
+            height: 22,
+            fillStyle: defaultFillStyle
+        },
+        keyPosition: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'ArrowRight',
+            x: 175 + 225 / 2 - 30,
+            y: moveLeftRow.text.y + textTopSpace
+        }
+    };
+
+
+    const moveUpRow = {
+        direction: 'UP',
+        isHighlighted: false,
+        text: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'MOVE UP:',
+            x: 40,
+            y: moveRightRow.text.y + textTopSpace
+        },
+        controls: {
+            x: 175,
+            y: moveRightRow.controls.y + rectTopSpace,
+            width: 225,
+            height: 22,
+            fillStyle: defaultFillStyle
+        },
+        keyPosition: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'ArrowUp',
+            x: 175 + 225 / 2 - 30,
+            y: moveRightRow.text.y + textTopSpace
+        }
+    };
+
+    const moveDownRow = {
+        direction: 'DOWN',
+        isHighlighted: false,
+        text: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'MOVE DOWN:',
+            x: 40,
+            y: moveUpRow.text.y + textTopSpace
+        },
+        controls: {
+            x: 175,
+            y: moveUpRow.controls.y + rectTopSpace,
+            width: 225,
+            height: 22,
+            fillStyle: defaultFillStyle
+        },
+        keyPosition: {
+            font: "16px Arial",
+            color: "#FFFFFF",
+            text: 'ArrowDown',
+            x: 175 + 225 / 2 - 30,
+            y: moveUpRow.text.y + textTopSpace
+        }
+    };
+
+    const table = [moveLeftRow, moveRightRow, moveUpRow, moveDownRow];
+
+    function initialize() {
+        document.addEventListener('keydown', handleKeyDown);
+        canvas.addEventListener('click', handleMouseClick);
+        canvas.addEventListener('mousemove', handleMouseMove)
+    }
+
+    function getMousePos(event) {
+        // http://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+        let rect = canvas.getBoundingClientRect(), // abs. size of element
+            scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+            scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+
+        return {
+            x: (event.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+            y: (event.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+        }
+    }
+
+    function willDispear() {
+        document.removeEventListener('keydown', handleKeyDown);
+        canvas.removeEventListener('click', handleMouseClick);
+
+        table.forEach(function(row) {
+            row.isHighlighted = false;
+        })
+    }
+
+    function handleMouseClick(event) {
+        table.forEach(function(row) {
+            if (isMouseWithinRow(event, row)) {
+                row.controls.fillStyle = highlightFillStyle;
+                row.isHighlighted = true;
+            } else {
+                row.controls.fillStyle = defaultFillStyle;
+                row.isHighlighted = false;
+            }
+        });
+    }
+
+    function isMouseWithinRow(event, row) {
+        let coord = getMousePos(event);
+        let mouseX = coord.x;
+        let mouseY = coord.y;
+        if (mouseX > row.controls.x && mouseX < row.controls.x + row.controls.width && mouseY > row.controls.y && mouseY < row.controls.y + row.controls.height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function handleMouseMove(event) {
+        table.forEach(function(row) {
+            if (!row.isHighlighted) {
+                if (isMouseWithinRow(event, row)) {
+                    row.controls.fillStyle = highlightFillStyle;
+                } else {
+                    row.controls.fillStyle = defaultFillStyle;
+                }
+            }
+        });
+    }
+
+    function handleKeyDown(event) {
+        let row = getHighlightedRow();
+        row.keyPosition.text = event.key;
+    }
+
+    function getHighlightedRow() {
+        for (let i = 0; i < table.length; i++) {
+            if (table[i].isHighlighted === true) {
+                return table[i];
+            }
+        }
+
+        return {};
+    }
+
+    function render() {
+        Graphics.drawText(moveLeftRow.text);
+        Graphics.drawRectangle(moveLeftRow.controls);
+        Graphics.drawText(moveRightRow.text);
+        Graphics.drawRectangle(moveRightRow.controls);
+
+        Graphics.drawText(moveUpRow.text);
+        Graphics.drawRectangle(moveUpRow.controls);
+
+        Graphics.drawText(moveDownRow.text);
+        Graphics.drawRectangle(moveDownRow.controls);
+
+        Graphics.drawText(moveLeftRow.keyPosition);
+        Graphics.drawText(moveRightRow.keyPosition);
+        Graphics.drawText(moveUpRow.keyPosition);
+        Graphics.drawText(moveDownRow.keyPosition);
+
+
+        Graphics.drawText(music);
+    }
+
+    return {
+        update: update,
+        render: render,
+        initialize: initialize,
+        willDispear: willDispear
+    }
+
+}());
