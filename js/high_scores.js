c07e0ce1e57f1c545d8312bef6c72bca4898f43f
Francisco <fjab92@gmail.com>
2017-04-19 00:11:09 -0600

    Implementing high score
    The high scores iffy can now add and get scores from the server. Next step is the populate it in the table

--- js/high_scores.js
+++ js/high_scores.js
@@ -2,6 +2,7 @@ let HighScores = (function() {
 
     const widthOfTable = Graphics.width - 60;
     const heightOfTable = Graphics.height - 60;
+    let scores = [];
     
     const table = {
         lineWidth: 1,
@@ -16,37 +17,78 @@ let HighScores = (function() {
     }
 
     function render() {
-
-        // Draw the table
         Graphics.drawUnFilledRectangle(table);
-        Graphics.drawUnFilledRectangle({
-            lineWidth: 1,
-            strokeStyle: 'red',
-            x: 30,
-            y: 30,
-            width: widthOfTable / 3,
-            height: heightOfTable / 5
+
+        for (let i = 0; i < scores.length; i++) {
+
+            // user
+            Graphics.drawUnFilledRectangle({
+                lineWidth: 1,
+                strokeStyle: 'red',
+                x: 30,
+                y: 30 + height * i,
+                width: width,
+                height: height
+            });
+
+            // score
+            Graphics.drawUnFilledRectangle({
+                lineWidth: 1,
+                strokeStyle: 'red',
+                x: width + 30,
+                y: 30 + height * i,
+                width: width,
+                height: height
+            });
+
+            // date
+            Graphics.drawUnFilledRectangle({
+                lineWidth: 1,
+                strokeStyle: 'red',
+                x: width * 2 + 30,
+                y: 30 + height * i,
+                width: width,
+                height: height
+            });
+        }
+    }
+
+    const width = widthOfTable / 3;
+    const height = heightOfTable / 5;
+
+    // $(document).ready(function() {
+    //     $.getJSON('./db/scores.json', function(data) {
+    //         scores = data.all;
+    //         console.log(scores);
+    //     });
+
+    //     // $.post("./db/scores.json", function(data, status){
+    //     //     alert("Data: " + data + "\nStatus: " + status);
+    //     // });
+
+    //     // $.post('./db/scores.json', {"something": 123});
+
+    //     // submitHighscore();
+    // })
+
+    // post("http://localhost:3000/db/scores.json"
+    function add(name, score, date) {
+        // $.post("/scores/add/" + name + "/" + score + "/" + date, { name: "John", time: "2pm" })
+        //     .done(function( data ) {
+        //         console.log("success");
+        // });
+
+        $.post("/scores/add/" + name + "/" + score + "/" + date).done(function(data){
+            console.log('Scored saved!');
         });
+        fetchScoresFromServer();
+    }
 
-        Graphics.drawUnFilledRectangle({
-            lineWidth: 1,
-            strokeStyle: 'red',
-            x: widthOfTable / 3 + 30,
-            y: 30,
-            width: widthOfTable / 3,
-            height: heightOfTable / 5,
+    function fetchScoresFromServer() {
+        $.getJSON('./db/scores.json', function(data) {
+            scores = data.all;
+            console.log(scores);
         });
-
-        Graphics.drawUnFilledRectangle({
-            lineWidth: 1,
-            strokeStyle: 'red',
-            x: widthOfTable / 3 * 2 + 30,
-            y: 30,
-            width: widthOfTable / 3,
-            height: heightOfTable / 5,
-        });
-
-
     }
 
     return {
--- server.js
+++ server.js
@@ -1,62 +1,52 @@
-/*
-    To install packages
-    > npm install
 
-    To start server.
-    > node server.js
-
-*/
-
-var scores = [{
-    user: 'Billy',
-    score: 123,
-    date: 'today'
-}, {
-    user: 'Sally',
-    score: 321,
-    date: 'yesterday'
-}];
-
+let fs = require('fs');
 let express = require('express');
 let app = express();
-let server = app.listen(3000, handleRequest);
+// let server = app.listen(3000, handleRequest);
+let http = require('http').createServer(app);
 
-function handleRequest() {
-    console.log('server is listening on port 3000...');
-}
 
-app.use(express.static('./'));
+let data = fs.readFileSync('./db/scores.json'); // Want to use sync version
+let scores = JSON.parse(data);
 
-// http://localhost:3000/highscores/abc/5
-app.get('/highscores/:something/:num', (request, response) => {
-    let data = request.params;
-    const n = data.num;
-    let reply = "";
-    for (let i = 0; i < n; i++) {
-        reply += 'i love ' + data.something + ' too yay<br/>';
-    }
-
-    response.send(reply);
+// app.use(express.static('./'));
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html');
 });
 
-// http://localhost:3000/scores/fran/10/today
-app.get('/scores/:user/:score/:date', (request, response) => {
+app.use('/', express.static(__dirname + '/'));
+
+http.listen(3000);
+
+// // http://localhost:3000/scores/all
+app.get('/scores/all', (request, response) => {
+    response.send(scores);
+});
+
+app.post('/scores/add/:user/:score/:date', (request, response) => {
     const params = request.params;
     const user = params.user;
     const score = Number(params.score);
     const date = params.date;
-    scores.push({
-        'user': user,
-        'score': score,
-        'date': date
+    
+    scores.all.push({
+        "user": user,
+        "score": score,
+        "date": date
     });
 
-    const reply = "Thank you";
+    const data = JSON.stringify(scores, null, 4);  // Do not resign to scores
 
-    response.send(reply);
-})
+    fs.writeFile('./db/scores.json', data, (error) => {
+        const reply = {
+            status: "success",
+            'user': user,
+            "score": score,
+            "date": date
+        };
 
-// http://localhost:3000/scores/all
-app.get('/scores/all', (request, response) => {
-    response.send(scores);
+        response.send(reply);
+    });
 });
+
+console.log('server is listening on port 3000...');
